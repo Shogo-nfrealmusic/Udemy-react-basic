666677e5c13f6232557781df01e86a6043cfb089
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-10-31 01:00:55 -0700

    basic of jsx

--- 04_react_basic/src/075_fragment/start/components/Child.jsx
+++ 04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -1,10 +1,15 @@
 import "./Child.css";
 
+
 const Child = () => {
   return (
+    <>
     <div className="component">
       <h3>Hello Component</h3>
     </div>
+    <h3>Hello Fragment</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus debitis tempore voluptatem perferendis culpa nam fugiat, commodi, iste tempora architecto, nostrum nesciunt optio eligendi ipsam aspernatur. Libero, fuga. Provident, odio?</p>
+    </>
   );
 };
 
