d580ab48ef6959c2ceaa5b55fe09a6de1709ce9d
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-11-06 18:59:13 -0800

    unique key

--- 06_control_and_form/src/010_list_components/start/Example.jsx
+++ 06_control_and_form/src/010_list_components/start/Example.jsx
@@ -1,14 +1,14 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const helloAnimals = animals.map((animal, index) => (
+    <li key={`${animal}-${index}`}>Hello, {animal}</li>
+  ));
 
   return (
     <>
       <h3>配列の操作</h3>
-      <ul>
-        
-      </ul>
+      <ul>{helloAnimals}</ul>
     </>
   );
 };
