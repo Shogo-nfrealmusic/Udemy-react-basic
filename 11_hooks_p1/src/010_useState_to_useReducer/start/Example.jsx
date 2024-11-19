662761ce9138e63ff18cef888e82b538730cad49
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-11-18 17:24:00 -0800

    useReducer

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,7 +1,35 @@
+import { useReducer, useState } from "react";
+
 const Example = () => {
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev, action) => {
+    if(action === '+'){
+      return ++prev;
+    } else {
+      return --prev;
+    }
+  }, 0)
+  const countUp = () => {
+    setState(prev => prev + 1);
+  }
+  const rcountUp = () => {
+    dispatch('+');
+  }
+  const rcountDown = () => {
+    dispatch('-');
+  }
   
   return (
     <>
+    <div>
+      <h3>{state}</h3>
+      <button onClick={countUp}>+</button>
+    </div>
+    <div>
+      <h3>{rstate}</h3>
+      <button onClick={rcountUp}>+</button>
+      <button onClick={rcountDown}>-</button>
+    </div>
     </>
   );
 };
