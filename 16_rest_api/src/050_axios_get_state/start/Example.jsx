2a3d860406dac7fbe125507c48958c897ca7bd66
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-12-20 16:17:11 -0800

    method

--- 16_rest_api/src/050_axios_get_state/start/Example.jsx
+++ 16_rest_api/src/050_axios_get_state/start/Example.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Example = () => {
-  return <></>;
+  const [users, setUsers] = useState([]);
+  useEffect( () => {
+    const getUser = async() => {
+      const res = await axios.get('http://localhost:3003/user')
+      setUsers(res.data);
+    }
+    getUser();
+  }, [])
+  return (
+    <div>
+      {users.map(user => {
+        return (
+          <div key={user.id}>
+            <h3>name: {user.username}</h3>
+            <h3>age: {user.age}</h3>
+            <h3>hobby: {user.hobbies.join(',')}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Example;
