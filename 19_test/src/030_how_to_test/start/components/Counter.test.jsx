a6a961d40e42477bfd6e2c52796701baaff77ffc
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2025-01-03 17:29:22 -0800

    arrage, act, assertion

--- 19_test/src/030_how_to_test/start/components/Counter.test.jsx
+++ 19_test/src/030_how_to_test/start/components/Counter.test.jsx
@@ -3,4 +3,20 @@ Arrage:(テストデータ、条件、環境の設定).
 Act:(ロジックの実行、関数の実行).
 Assertion:(実行結果と期待する結果の比較).
 */
-test('ダミーテスト:削除して記載してください。', () => {});
+
+import { render, screen, fireEvent } from "@testing-library/react";
+import Counter from "./Counter";
+
+
+test('CountUp button Current Count + 1', () => {
+    render(<Counter originCount={0} />);
+
+    const spanElBefore = screen.getByText('現在のカウント:0')
+    expect(spanElBefore).toBeInTheDocument();
+
+    const btn = screen.getByRole('button', { name: 'カウントアップ'});
+    fireEvent.click(btn);
+
+    const spanEl = screen.getByText('現在のカウント:1')
+    expect(spanEl).toBeInTheDocument();
+});
