4f309ccb2493a9984f507d36253dcc6eb53920d3
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2025-01-08 16:18:27 -0800

    obj, array

--- 20_typescript/src/050_objects/start/Example.tsx
+++ 20_typescript/src/050_objects/start/Example.tsx
@@ -1,5 +1,13 @@
 const Example = () => {
-  // 練習用
+  const arry1: number[] = [1,2,3];
+  console.log(arry1);
+  const arry2: string[] = ['a', 'b', 'c'];
+  console.log(arry2);
+  const arry3: Array<string> = ['a', 'b', 'c'];
+  console.log(arry3);
+
+  const obj1: { name: string, age: number} = { name: 'shogo', age: 20};
+  console.log(obj1);
 };
 
 export default Example;
