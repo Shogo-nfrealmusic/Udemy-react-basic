d41d059cfc9cd7fd5dfb112e0534e7cfedf63540
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2025-01-08 16:29:57 -0800

    function

--- 20_typescript/src/080_function_type/start/Example.tsx
+++ 20_typescript/src/080_function_type/start/Example.tsx
@@ -1,5 +1,18 @@
+
+
 const Example = () => {
-  // 練習用
+  function sum1(x: number, y: number){
+    return x + y;
+  };
+  const result1 = sum1(1,2);
+  console.log(result1);
+
+
+  const sum2 = (x: number, y: number) => {
+    return x + y;
+  };
+  const result2 = sum2(1,2);
+  console.log(result2);
 };
 
 export default Example;
