4670560b6367f134bc1a1cfad6aaffc26d62c434
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2025-01-08 16:07:47 -0800

    primitive

--- 20_typescript/src/030_primitives/start/Example.tsx
+++ 20_typescript/src/030_primitives/start/Example.tsx
@@ -1,5 +1,25 @@
 const Example = () => {
-  // 練習用
+  let str: string = 'Hello';
+  str = 'Bye';
+  console.log(str);
+
+  let num: number = 123;
+  console.log(num);
+
+  let bignum: bigint = 103n;
+  console.log(bignum);
+
+  let bool: boolean = true;
+  console.log(bool);
+
+  let nullish: null = null;
+  let undefinedValue: undefined = undefined;
+  let trueVal: true = true;
+  console.log(nullish);
+  console.log(undefinedValue);
+  console.log(trueVal);
+
+
 };
 
 export default Example;
