7e48941b112fae108a8ad16f3a70206ea117c448
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2025-01-11 19:32:50 -0800

    props

--- 20_typescript/src/120_props/start/Example.tsx
+++ 20_typescript/src/120_props/start/Example.tsx
@@ -1,5 +1,12 @@
+import Hello, { Btn } from "./Hello";
+
 const Example = () => {
-  // 練習用
+  return (
+    <>
+      <Hello text='Typescript'>children</Hello>
+      <Btn fn={(text) => console.log(text)}/>
+    </>
+);
 };
 
 export default Example;
