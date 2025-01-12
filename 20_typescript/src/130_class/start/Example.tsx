9f15e28dfa90c189e255d3dec72c7b6e2ff55898
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2025-01-11 19:38:33 -0800

    class

--- 20_typescript/src/130_class/start/Example.tsx
+++ 20_typescript/src/130_class/start/Example.tsx
@@ -1,5 +1,22 @@
+class User {
+  public name: string;
+  public age : number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
 const Example = () => {
-  // 練習用
+  // const user = { name: 'shogo', age: 20 }
+  const user1 = new User('shogo', 20);
+  return (
+    <div>
+      <p>Name: {user1.name}</p>
+      <p>Age: {user1.age}</p>
+    </div>
+  )
 };
 
 export default Example;
