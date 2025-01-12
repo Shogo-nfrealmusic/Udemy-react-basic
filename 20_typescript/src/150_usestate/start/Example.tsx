410f5a8066f5b2b55fa775f1dbb323b1eb3f5ebd
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2025-01-11 21:57:19 -0800

    typescript

--- 20_typescript/src/150_usestate/start/Example.tsx
+++ 20_typescript/src/150_usestate/start/Example.tsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
+type Todo = {
+    id: number,
+    text: string
+}
+
 const Example = () => {
-  
+//   const [text, setText] = useState('hello');
+const [todos, setTodos] = useState<Todo[]>([{ id:0, text: 'Homework'}])
 };
 
 export default Example;
--- 20_typescript/src/160_add_list_item/start/Example.tsx
+++ 20_typescript/src/160_add_list_item/start/Example.tsx
@@ -1,7 +1,29 @@
 import { useState } from "react";
+import TaskList from "./TaskList";
 
+
+export type Todo = {
+    id: number,
+    text: string
+}
 const Example = () => {
-  
+  const [todos, setTodos] = useState<Todo[]>([
+    {id: 0, text: 'Gym'},
+    {id: 1, text: 'Study'},
+  ]);
+  const [inputText, setInputText] = useState('')
+  const changeHandler = (e:React.ChangeEvent<HTMLInputElement>) => setInputText(e.target.value);
+  const addTodoItem = () => {
+    setTodos(state => [...state, { id: setTodos.length, text: inputText}]);
+    setInputText('');
+  }
+  return (
+    <>
+        <input type="text" value={inputText} onChange={changeHandler}/>
+        <button onClick={addTodoItem}>Add</button>
+        <TaskList todos={todos}/>
+    </>
+  )
 };
 
 export default Example;
